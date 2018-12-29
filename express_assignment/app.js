f81eccfe1e2ae59b49735c8ca0e36076f7f7ace7
olichid <zhengchengc@student.unimelb.edu.au>
2018-12-29 23:43:26 +1100

    version 2 of my solution

--- express_assignment/app.js
+++ express_assignment/app.js
@@ -6,16 +6,26 @@ app.get('/', function(req, res) {
     res.send("Hi there, welcome to my assignment!");
 });
 
-app.get('/speak/:animal', function(req, res) {
-    if (req.params.animal == "pig") {
-        res.send("The pig says 'Oink'");
-    } else if (req.params.animal == "cow") {
-        res.send("THe cow says 'Moo'");
-    } else if (req.params.animal == "dog") {
-        res.send("The dog says 'Woof Woof!'");
-    }
-});
+// app.get('/speak/:animal', function(req, res) {
+//     if (req.params.animal == "pig") {
+//         res.send("The pig says 'Oink'");
+//     } else if (req.params.animal == "cow") {
+//         res.send("THe cow says 'Moo'");
+//     } else if (req.params.animal == "dog") {
+//         res.send("The dog says 'Woof Woof!'");
+//     }
+// });
 
+app.get('/speak/:animal', function(req, res) {
+    let sound = {
+        pig: 'Oink',
+        cow: 'Moo',
+        dog: 'Woof Woof!'
+    }
+    let animal = req.params.animal;
+    let animalsound = sound[animal];
+    res.send("The " + animal + " says '" + animalsound + "'.");
+})
 app.get('/repeat/:greeting/:times', function(req, res) {
     let greetword = req.params.greeting;
     let repeattime = req.params.times;
@@ -29,4 +39,6 @@ app.get('/repeat/:greeting/:times', function(req, res) {
 app.get('*', function(req, res) {
     res.send("Sorry, page not found...What are you doing with your life?");
 })
-app.listen(3000);
+app.listen(port, function() {
+    console.log("The app is listening on port " + port + "!");
+});
