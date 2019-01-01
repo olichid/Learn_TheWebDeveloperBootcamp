a3f5f59b0e7b9c29d5f229b9bf1076e829b19325
olichid <zhengchengc@student.unimelb.edu.au>
2019-01-01 17:13:35 +1100

    RESTful INDEX

---
+++ RESTful Routing/RESTfulBlogApp/app.js
@@ -0,0 +1,50 @@
+let express     = require("express"),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    app         = express(),
+    port        = 3000;
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true});
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// MONGOOSE/MODEL CONFIG
+let blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: 
+    {
+        type: Date, 
+        default: Date.now
+    }
+});
+
+let Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//     title: "Test Blog",
+//     image: "https://sbb.imgix.net/content/dam/internet/keyvisual/GA-Hund.jpg?crop=focalpoint&fp-x=0.4981685&fp-y=0.25&fp-z=1&w=2656&h=960&auto=format,compress,cs=tinysrgb",
+//     body: "This is a golden retriever."
+// });
+// RESTFUL ROUTES
+app.get("/", function(req, res) {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res) {
+    Blog.find({}, function(err, blogs) {
+        if (err) {
+            console.log("ERROR!");
+        } else {
+            res.render("index", {blogs: blogs});
+            
+        }
+    })
+});
+
+app.listen(port, function() {
+    console.log("App Server is Working!");
+})
